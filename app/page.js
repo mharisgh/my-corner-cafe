2ae0ff03ea9e81fb03ea68c360b751f9b3a3e18d
mharisgh <hellomharis@gmail.com>
2024-02-01 18:32:06 +0530

    menu

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ const loveLight = Love_Light({
 
 export default function Home() {
   return (
-    <main className="slide-up max-w-2xl flex flex-col mx-auto items-center gap-4">
+    <main className="slide-up mt-10 max-w-2xl flex flex-col mx-auto items-center gap-4">
       <img className="max-w-[200px] " src="/images/logo-mycorner.png" alt="logo" />
       <img className="max-w-[400px]" src="/images/menu-bg.webp" alt="menu" />
       <Link href='/category' className="text-5xl hover:text-[#3e2f25]">
