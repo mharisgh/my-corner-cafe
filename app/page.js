0606da7076c0adc040e9403e56a0181395c818b8
mharisgh <hellomharis@gmail.com>
2024-02-03 22:37:08 +0530

    menu-button fix

--- app/page.js
+++ app/page.js
@@ -13,7 +13,8 @@ export default function Home() {
     <main className="slide-up mt-10 max-w-2xl flex flex-col mx-auto items-center gap-4">
       <img className="max-w-[200px] " src="/images/logo-mycorner.png" alt="logo" />
       <img className="max-w-[400px]" src="/images/menu-bg.webp" alt="menu" />
-      <Link href='/category' className="text-5xl hover:text-[#3e2f25]">
+      <Link href='/category' 
+      className="text-5xl bg-[#4a3c34]/10 hover:bg-[#4a3c34]/20  px-6 py-3 rounded-2xl hover:text-[#3e2f25]">
         <h1 className={loveLight.className}>Menu</h1>
       </Link>
     </main>
